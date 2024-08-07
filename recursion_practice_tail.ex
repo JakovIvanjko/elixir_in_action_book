e04a179c5b3326ecbef06394596c0b5d8dd75f22
JakovIvanjko <jakov.ivanjko@gmail.com>
2024-08-07 10:15:07 +0200

    Removed IO.puts()

--- recursion_practice_tail.ex
+++ recursion_practice_tail.ex
@@ -16,21 +16,22 @@ defmodule Practice do
 
 
     def range(from, to) do
-        do_range(from, to)
+
+        do_range(from, to, [])
     end
 
-    defp do_range(to, to) do
-        to
+    defp do_range(to, to, l) do
+        l = l ++ [to]
     end
 
-    defp do_range(from, to) when to > from do
-        IO.puts(from)
-        do_range(from+1, to)
+    defp do_range(from, to, l) when to > from do
+        l = l ++ [from]
+        do_range(from+1, to, l)
     end
     
-    defp do_range(from, to) when to < from do
-        IO.puts(from)
-        do_range(from-1, to)
+    defp do_range(from, to, l) when to < from do
+        l = l ++ [from]
+        do_range(from-1, to, l)
     end
 
     def positive(list) do
