dbf8d8dae7dfa784193f2d793dc23a4045a93e11
JakovIvanjko <jakov.ivanjko@gmail.com>
2024-08-13 12:52:10 +0200

    Render function optimization

--- phoenix_project/pento/lib/pento_web/live/wrong_live.ex
+++ phoenix_project/pento/lib/pento_web/live/wrong_live.ex
@@ -5,26 +5,6 @@ defmodule PentoWeb.WrongLive do
         {:ok, assign(socket, score: 0, goal: Enum.random(1..10), objective: :false,  message: "Make a guess:")}
     end
 
-    def render(assigns) when assigns.objective == :true do
-        ~H"""
-        <h2>
-            It's <%= time() %>
-        </h2>
-
-        <h1 class="mb-4 text-4xl font-extrabold">Your score: <%= @score %></h1>
-        <h2>
-            <%= @message %>
-        </h2>
-        <br/>
-        <h2>
-            <.link href="http://127.0.0.1:4000/guess" class="bg-blue-500 hover:bg-blue-700
-                text-white font-bold py-2 px-4 border border-blue-700 rounded m-1"
-                phx-click="Restart"  >
-                    <%= "Restart" %>
-            </.link>
-        </h2>
-        """
-    end
 
     def render(assigns) do
         ~H"""
@@ -37,17 +17,28 @@ defmodule PentoWeb.WrongLive do
             <%= @message %>
         </h2>
         <br/>
-        <h2>
-            <%= for n <- 1..10 do %>
-                <.link href="#" class="bg-blue-500 hover:bg-blue-700
-                text-white font-bold py-2 px-4 border border-blue-700 rounded m-1"
-                phx-click="guess" phx-value-number= {n} >
-                    <%= n %>
-                </.link>
-            <% end %>
-        </h2>
-    
 
+        <%= if @objective do%>
+
+            <h2>
+                <.link href="http://127.0.0.1:4000/guess" class="bg-blue-500 hover:bg-blue-700
+                    text-white font-bold py-2 px-4 border border-blue-700 rounded m-1"
+                    phx-click="Restart"  >
+                        <%= "Restart" %>
+                </.link>
+            </h2>
+        <%else%>
+            <h2>
+            
+                <%= for n <- 1..10 do %>
+                    <.link href="#" class="bg-blue-500 hover:bg-blue-700
+                    text-white font-bold py-2 px-4 border border-blue-700 rounded m-1"
+                    phx-click="guess" phx-value-number= {n} >
+                        <%= n %>
+                    </.link>
+                <% end %>
+            </h2>
+        <%end%>
         """
     end
 
